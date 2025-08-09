2c3c29f4ce9b48b815fed8915001e589ed1608a0
Alfrin Poulose <alfrinpoulose@gmail.com>
2025-08-10 02:16:17 +0530

    feat: enhance blog page layout with author details, category badges, and improved data structure

--- app/[blogId]/page.tsx
+++ app/[blogId]/page.tsx
@@ -1,9 +1,44 @@
 "use client";
 
 import { useGetBlogs } from "@/app/api/posts/query";
+import Badge from "@/components/Badge";
+import { blogsDataList } from "@/constants/data";
+import { dataTagErrorSymbol } from "@tanstack/react-query";
+import Image from "next/image";
 
 export default function Blog(params: { blogId: string }) {
   const { blogId } = params;
-  const { isPending, isError, data, error } = useGetBlogs(blogId);
-  return <div>page</div>;
+  // const { isPending, isError, data, error } = useGetBlogs(blogId);
+  return (
+    <div className="max-w-7xl w-full mx-auto p-4.5 space-y-4.5">
+      <h1 className="text-7xl">{blogsDataList[0].title}</h1>
+      <div className="flex gap-5 items-center">
+        <div className="flex items-center justify-start gap-2">
+          <Image
+            src={blogsDataList[0].author.profilePic}
+            alt={blogsDataList[0].author.authorName}
+            width={100}
+            height={100}
+            className="rounded-full size-7 object-cover"
+          />
+          <span className="text-gray-700">
+            {blogsDataList[0].author.authorName}
+          </span>
+          <div className="rounded-full size-1 bg-gray-700"></div>
+          <span className="text-gray-700">{`${blogsDataList[0].timeToRead} min read`}</span>
+        </div>
+        <div className="flex-1 h-[1px] bg-gray-700 rounded-2xl"></div>
+        <Badge text={blogsDataList[0].category} />
+      </div>
+      <Image
+        src={blogsDataList[0].img}
+        alt={blogsDataList[0].title}
+        width={1000}
+        height={1000}
+        className="w-full rounded-2xl h-[500px] object-cover"
+      />
+
+      <p className="text-xl text-left ">{blogsDataList[0].data}</p>
+    </div>
+  );
 }
--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,6 +1,6 @@
 export default function Badge({ text }: { text: string }) {
   return (
-    <span className="px-2 py-1 text-xs text-black rounded-full border-[1px] border-white">
+    <span className="px-2 py-1 text-xs text-black rounded-full border-[1px] border-gray-400">
       {text}
     </span>
   );
