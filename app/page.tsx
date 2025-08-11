489aa73051facb9ba4f8993f1f8e0f1fadd1e279
Alfrin Poulose <alfrinpoulose@gmail.com>
2025-08-12 00:42:56 +0530

    feat: restructure home page layout by integrating NavBar, FeaturedBlogs, and RecentBlogs components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import NavBar from '@/components/NavBar';
+import FeaturedBlogs from './sections/FeaturedBlogs';
+import RecentBlogs from './sections/RecentBlogs';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="max-w-7xl w-full mx-auto py-4.5">
+      <NavBar />
+      <div className="px-4.5 space-y-5 md:space-y-10">
+        <FeaturedBlogs />
+        <RecentBlogs />
+      </div>
+    </div>
+  );
 }
